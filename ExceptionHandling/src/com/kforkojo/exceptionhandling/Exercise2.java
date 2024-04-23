918e83e76d985296bc22f879c853b558a09f251e
Joseph Ampadu <jkampadujnr@gmail.com>
2024-04-23 10:16:08 +0000

    add Wk1 Lab1 Exceptions Ex2

---
+++ ExceptionHandling/src/com/kforkojo/exceptionhandling/Exercise2.java
@@ -0,0 +1,30 @@
+package com.kforkojo.exceptionhandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Exercise2 {
+
+    public static void main(String[] args) throws Exception {
+        String filename = "numbers.txt";
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+
+            try {
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println("Number: " + Integer.parseInt(line));
+                }
+            } catch (NumberFormatException ex) {
+                System.out.println("Error parsing line: " + ex.getMessage());
+            } finally {
+                reader.close();
+            }
+        } catch (IOException ex) {
+            System.out.println("Error processing file: " + ex.getMessage());
+        }
+    }
+}
